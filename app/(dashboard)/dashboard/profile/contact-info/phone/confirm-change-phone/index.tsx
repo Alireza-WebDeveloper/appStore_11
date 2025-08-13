0c6cb96daf0a4f90baf8d9b3e5daa8a30deffbd1
Alireza-WebDeveloper <alirezakh0090@gmail.com>
2025-08-13 11:30:10 +0430

    update dashboard routes

---
+++ app/(dashboard)/dashboard/profile/contact-info/phone/confirm-change-phone/index.tsx
@@ -0,0 +1,5 @@
+const ConfirmToChangePhone = () => {
+  return <form onSubmit={() => {}} className="space-y-8"></form>;
+};
+
+export default ConfirmToChangePhone;
---
+++ app/(dashboard)/dashboard/profile/contact-info/phone/request-to-change-phone/index.tsx
@@ -0,0 +1,5 @@
+const RequestToChangePhone = () => {
+  return <form onSubmit={() => {}} className="space-y-8"></form>;
+};
+
+export default RequestToChangePhone;
