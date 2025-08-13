98cac4e4955141de9d497c6fe1661ba2dd5d54c5
Alireza-WebDeveloper <alirezakh0090@gmail.com>
2025-08-13 09:57:43 +0430

    update dashboard/profile

--- app/(dashboard)/components/SideBar/Profile/index.tsx
+++ app/(dashboard)/components/SideBar/Profile/index.tsx
@@ -1,12 +1,17 @@
 import { PersonIcon } from '@/app/lib/design/common/icons';
+import { useFetchProfile } from '@/app/lib/hooks/auth';
 
 const Profile = () => {
+  const { data: profile } = useFetchProfile();
+
   return (
     <div className="flex items-center gap-3 mb-6 px-2">
       <PersonIcon width={20} height={20} />
       <div className="flex flex-col">
-        <span className="text-sm font-semibold">{/* Name */}</span>
-        <span className="text-xs text-white/50">{/* Email*/} </span>
+        <span className="text-sm font-semibold">
+          {profile?.user.firstName} {profile?.user.lastName}
+        </span>
+        <span className="text-xs text-white/50">{profile?.user.email} </span>
       </div>
     </div>
   );
